36a7ec3b315ed5b63d368756ce60a9843c9ccbdd
Ashley Levi <Ashplevi@gmail.com>
2018-12-04 21:52:31 -0700

    css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import './Header.css'
 
 
 export const Header = () => {
   return (
     <div className="header">
-        <h1>SWAPIBOX</h1>
+        <h1>SWAPI BOX</h1>
         <button>Favorites</button>
       </div>
   )
