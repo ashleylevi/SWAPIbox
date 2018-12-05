c0ef42ed33727f31e79e3f937c5db04df07dd69e
Ashley Levi <Ashplevi@gmail.com>
2018-12-04 17:51:03 -0700

    Add header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+export const Header = () => {
+  return (
+    <div className="header">
+        <h1>SWAPIBOX</h1>
+        <button>Favorites</button>
+      </div>
+  )
+}
