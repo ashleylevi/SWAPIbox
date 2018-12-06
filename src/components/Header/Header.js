610547b963381f7619d910e17e150bfc1def4c73
Ashley Levi <Ashplevi@gmail.com>
2018-12-06 14:58:03 -0700

    Fix font

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,8 +5,9 @@ import './Header.css'
 export const Header = () => {
   return (
     <div className="header">
-        <img className="header-pic" src="https://farm5.staticflickr.com/4864/46147749322_622c507324.jpg"></img>
-        <h1 className="main-title">SWAPI BOX</h1>
+      <div className="img-div"><img className="header-pic" src="https://farm5.staticflickr.com/4864/46147749322_622c507324.jpg"></img>
+      </div>
+      <h1 className="main-title">SWAPi Box</h1>
       </div>
   )
 }
