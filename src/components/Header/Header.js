eee39888568588ad1d392e8db0bb1515458eca53
Ashley Levi <Ashplevi@gmail.com>
2018-12-05 20:26:22 -0700

    Add yellow css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ export const Header = () => {
   return (
     <div className="header">
         {/* <img className="header-pic" src="https://farm5.staticflickr.com/4892/32323633148_5da9c3ab4b.jpg"></img> */}
-        <h1>SWAPI BOX</h1>
+        <h1 className="main-title">SWAPI BOX</h1>
       </div>
   )
 }
