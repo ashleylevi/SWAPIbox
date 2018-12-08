54c33a11d2093d7fd912617c128925c6c27d7597
Tanjie McMeans <tanjmcmeans01@gmail.com>
2018-12-08 13:42:56 -0700

    Add remove localstorage function

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,16 +2,24 @@ import React from 'react';
 import './Nav.css';
 // import { prependOnceListener } from 'cluster';
 
-export const Nav = (props) => {
-  // let favorite;
-  // props.favoriteCards.length === 0 ? favorite = 0 : favorite = props.favoriteCards.length
+export const Nav = ({ fetchData, favoriteCards, setFavoriteData }) => {
+  let favorite;
+  favoriteCards.length === 0
+    ? (favorite = 0)
+    : (favorite = favoriteCards.length);
   return (
     <div>
       <section className="nav">
-        <button name="people" onClick={ props.fetchData}>PEOPLE</button>
-        <button name="planets" onClick={props.fetchData}>PLANETS</button>
-        <button name="vehicles" onClick={props.fetchData}>VEHICLES</button>
-        <button>FAVORTIES: </button>
+        <button name="people" onClick={fetchData}>
+          PEOPLE
+        </button>
+        <button name="planets" onClick={fetchData}>
+          PLANETS
+        </button>
+        <button name="vehicles" onClick={fetchData}>
+          VEHICLES
+        </button>
+        <button onClick={setFavoriteData}>FAVORTIES: {favorite}</button>
       </section>
       <section className="background-fade" />
     </div>
