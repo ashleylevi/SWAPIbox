2d3d5f46177fb2a76a3277eae2a08409eb528b94
Ashley Levi <Ashplevi@gmail.com>
2019-01-13 17:42:21 -0700

    Updates

---
+++ precache-manifest.8306f809bbe837af53aa9de4aa0ce03d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c982ef251eeaa93f501080ca646e5e24",
+    "url": "/SWAPIbox/static/media/anakinmono.c982ef25.ttf"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/SWAPIbox/static/media/Starjedi.cc3019aa.ttf"
+  },
+  {
+    "revision": "e6589c20186e3c681101",
+    "url": "/SWAPIbox/static/js/runtime~main.e6589c20.js"
+  },
+  {
+    "revision": "fc6ee16170e11f278c17",
+    "url": "/SWAPIbox/static/js/main.fc6ee161.chunk.js"
+  },
+  {
+    "revision": "9c544aa8fa2231700ef0",
+    "url": "/SWAPIbox/static/js/1.9c544aa8.chunk.js"
+  },
+  {
+    "revision": "fc6ee16170e11f278c17",
+    "url": "/SWAPIbox/static/css/main.6f82b38a.chunk.css"
+  },
+  {
+    "revision": "2a46964ee7b02d1df552fed690439489",
+    "url": "/SWAPIbox/index.html"
+  }
+];
